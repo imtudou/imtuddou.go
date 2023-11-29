63659eec85de695ee4105de8a211a8cbbf2464b9
yuanyi <imtudous@163.com>
2023-11-29 14:13:18 +0800

    字符串中常用系统函数

--- src/sample/chapter5/demo07_函数中defer/main.go
+++ src/sample/chapter5/demo07_函数中defer/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+//当执行到defer时，暂时不执行，会将defer后面的语句压入到独立的栈（defer栈）
+//当函数执行完毕后，再从defer栈，按照先入后出的方式出栈，执行
 func sum(n1 int, n2 int) int {
 	defer fmt.Println("sum.n1 = ", n1)
 	defer fmt.Println("sum.n2 = ", n2)
@@ -22,3 +24,7 @@ func main() {
 	// sum.n1 =  11
 	// main.res =  34
 }
+
+// defer 注意事项和细节
+// 1)值类型:基本数据类型int系列, float系列,bool, string 、数组和结构体struct
+// 2)引用类型：指针、slice 切片、map、管道 chan、 interface 等都是引用类型
---
+++ src/sample/chapter5/demo08_字符串中常用系统函数/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//1.统计字符串的长度，按字节 len(str)
+	//golang的编码统一为utf-8 （ascii的字符（字母和数字）占一个字节，汉字占用3个字节
+	str := "上海shanghai"
+	fmt.Println("str len=", len(str))
+
+	//2.字符串遍历，同时处理有中文的问题 r ：= []rune(str)
+	str2 := "上海shanghai"
+	r := []rune(str2)
+	for i := 0; i < len(r); i++ {
+		fmt.Printf("字符=%c\n", r[i])
+	}
+
+	//3.字符串转整数：n, err := strconv.Atoi("12")
+	n, err := strconv.Atoi("1bnm,")
+	fmt.Printf("n=%v,err=%v \n", n, err)
+
+	//4)整数转字符串 str = strconv.Itoa(12345)
+	str = strconv.Itoa(12345)
+	fmt.Printf("str=%v\n", str)
+
+	//5字符串 转 []byte： var bytes = []byte("hello go")
+	var bytes = []byte("hello go")
+	fmt.Printf("bytes=%v\n", bytes)
+
+	//6)[]byte 转 字符串： str = string([]byte{97, 98, 99})
+	str = string([]byte{97, 98, 99})
+	fmt.Println("str=", str)
+
+	//7.查找子串是否在指定的字符串中：strings.Contains(“seafood"，"foo")//true
+	fmt.Println(strings.Contains("seafood", "foo"))
+
+	//8.统计一个字符串有几个指定的子串 ： strings.Count("ceheese"，"e")//4
+	fmt.Println(strings.Count("ceheese", "e"))
+
+	//10）不区分大小写的字符串比较(==是区分字母大小写的）：fmt.Println（strings.EqualFold（"abc"，Abc)
+	fmt.Println(strings.EqualFold("abc", "Abc"))
+
+	//11)返回子串在字符串第一次出现的index值，如果没有返回-1//strings.Index("NLT abc", "abc") // 4
+	fmt.Println(strings.Index("NLT abc", "abc"))
+
+	//12)返回子串在字符串最后一次出现的 index，如没有返回-1: strings.LastIndex("go golang"，"go"
+
+	fmt.Println(strings.LastIndex("golang", "g"))
+
+	//13)将指定的子串替换成另外一个子串: strings Replace("go go hello", "go", "go语言", n) n可以指定你希望替换几个，如果 n=-1 表示全部替换
+
+	fmt.Println(strings.Replace("aaabbbccc", "b", "d", 1))
+	sstr := "aaabbbccc"
+	fmt.Println(strings.Replace(sstr, "b", "d", 1))
+
+	//14)按照指定的某个字符,为分割标识,将一个字符串拆分成字符串数组,strings.Split("hello,wrold,ok", ".")
+	arr := strings.Split("hello,wrold,ok", ",")
+	fmt.Println(arr)
+
+	//15)将字符串的字母进行大小写的转换: strings.ToLower(Go") // go strings.ToUpper("Go") // Go
+	fmt.Println(strings.ToLower("strings.ToLower"))
+	fmt.Println(strings.ToUpper("strings.ToUpper"))
+}
