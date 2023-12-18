d79e229a837553ca08b968a2e200b6c57bbc43c4
yuanyi <imtudous@163.com>
2023-12-18 18:28:49 +0800

    用工厂模式

--- src/sample/chapter9/demo02_Factory/main.go
+++ src/sample/chapter9/demo02_Factory/main.go
@@ -9,6 +9,11 @@ func main() {
 	stu := model.Student{Name: "tom", Age: 19}
 	fmt.Println(stu)
 
+	stu2 := model.CreateStudent("tom", 19, 11.2, 190.9)
+	fmt.Println(*stu2)
+
+	fmt.Println(stu2.GetHeight())
+
 }
 
 // Golang的结构体没有构造函数,通常可以使用工厂模式来解决这个问题。
--- src/sample/chapter9/model/model.go
+++ src/sample/chapter9/model/model.go
@@ -5,3 +5,26 @@ type Student struct {
 	Age   int
 	Score float64
 }
+
+type studentsss struct {
+	Name   string
+	Age    int
+	Score  float64
+	height float64
+}
+
+//因为student结构体首字母是小写，因此是只能在model使用//我们通过工厂模式来解决
+func CreateStudent(name string, age int, score float64, h float64) *studentsss {
+	return &studentsss{
+		Name:   name,
+		Age:    age,
+		Score:  score,
+		height: h,
+	}
+}
+
+//如果score字段首字母小写，则，在其它包不可以直接方法，我们可以提供一个方法
+
+func (s *studentsss) GetHeight() float64 {
+	return s.height
+}
